db557a51f95703341291090e7742efead9f96aab
aguilarm8295 <68097381+aguilarm8295@users.noreply.github.com>
2020-07-10 01:58:27 -0400

    First Submission
    First Submission

---
+++ P2T1_SalesPrediction_AguilarMiryan.py
@@ -0,0 +1,16 @@
+# Get the projected sales
+# 7/10/20
+# CTI-110 P2T1- Sales Prediction
+# Miryan Mendoza
+#
+
+
+# Get the projected sales
+total_sales = float(input('Enter the projected sales:'))
+
+# Calcuate the profit as 23 percent of total sales.
+profit = total_sales * .23
+
+# Display the profit.
+print('The profit is $',profit)
+
